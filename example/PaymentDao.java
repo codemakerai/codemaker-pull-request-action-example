13f3f231d8b5131bbb8b29c22f37882302a438fa
SaoYan <25550286+SaoYan@users.noreply.github.com>
2023-09-17 10:54:14 -0700

    Add sample java file

---
+++ example/PaymentDao.java
@@ -0,0 +1,35 @@
+public class PaymentDao {
+
+    private static final Logger logger = LoggerFactory.getLogger(PaymentDao.class);
+    
+    private final Session session;
+
+    public PaymentDao(Session session) {
+        this.session = session;
+    }
+
+    /**
+     * Persists the payment in the database.
+     *
+     * @param payment payment to save
+     * @throws IllegalArgumentException if payment is null
+     */
+    public void save(Payment payment) {
+    }
+
+    /**
+     * Updates the payment in the database.
+     *
+     * @param payment payment to save
+     */
+    public void update(Payment payment) {
+    }
+
+    /**
+     * Deletes the payment from the database.
+     *
+     * @param payment payment to save
+     */
+    public void delete(Payment payment) {
+    }
+}
