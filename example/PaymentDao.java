45d2701e5bd960e47570d89282c0acdf191b71d0
codemakerai-dev <codemaker-dev@codemaker.ai>
2023-09-23 23:37:58 -0700

    Demo

---
+++ example/PaymentDao.java
@@ -0,0 +1,31 @@
+public class PaymentDao {
+
+    private static final Logger logger = LoggerFactory.getLogger(PaymentDao.class);
+
+    private final Session session;
+
+    public PaymentDao(Session session) {
+        this.session = session;
+    }
+
+    /**
+     * Saves payment in the database.
+     * @param payment the payment to save
+     */
+    public void save(Payment payment) {
+
+    }
+
+    /**
+     * Updates payment in the database
+     * @param payment
+     */
+    public void update(Payment payment) {
+    }
+
+    public Payment get(String id) {
+    }
+
+    public void delete(Payment payment) {
+    }
+}
